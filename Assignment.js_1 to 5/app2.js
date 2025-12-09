48a8a1c1ed5dad3472cb1891c1bea7284ae20227
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2025-12-10 04:46:10 +0500

    Variables for string Javascript
    This file contains a series of prompts and alerts for user interaction, including name input, age, subject enrollment, and a fun pizza display.

---
+++ Assignment.js_1 to 5/app2.js
@@ -0,0 +1,41 @@
+//Que 01
+var username = "";
+
+//Que 02
+var myname = prompt("Enter You Full Name :");
+alert("Hello " + myname + "😊");
+console.log(myname);
+
+//Que 04
+var studentName = prompt("Enter Your Name :");
+var studentAge = prompt("Enter Your Age :");
+var studentSubject = prompt("Subject :");
+alert("Student's Name : " + studentName);
+alert("Student's Age : " + studentAge);
+alert("Student's Subjects : " + studentSubject);
+alert(studentName + " Thank you for enrolling in " + studentSubject);
+
+//Que 05
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert (pizza);
+
+//Que 06
+var email = prompt("Enter Your Email Address :");
+alert ("Your Email Address is : "+ email);
+
+//Que 07
+var book = "A smarter way to learn JavaScript";
+alert ("I'am Trying To learn from the book "+ book);
+
+//Que 08
+var message2 = "Yah! I can write HTML content through Javascript";
+document.write(message2);
+
+breakline= "<br><br>";
+document.write(breakline);
+
+
+//Que  09
+var design = "‿︵‿︵ʚɞ『Assignment Complete』ʚɞ‿︵‿︵";
+alert (design);
+document.write(design);
