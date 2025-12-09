97996d7842b3e9134b3a3f2280ca20c8609ff55d
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2025-12-10 02:56:18 +0500

    alerts javascript

---
+++ Assignment.js_1 to 5/app.js
@@ -0,0 +1,32 @@
+//Que 01
+var user = prompt("Enter Your Name:");
+alert("Hello " + user);
+
+//Que 02
+var rightPass = "Saylani";
+var wrongPass = "saylani";
+var userPrompt = prompt("Enter SMIT lab PC Password:");
+if (rightPass === userPrompt) {
+    alert("Correct Password!");
+}
+if (wrongPass === userPrompt) {
+    alert("Error! Enter Correct Password...");
+}
+userPrompt = prompt("Enter correct password:");
+alert("Correct password!");
+
+//Que 03
+var message = "Welcome to Winters \nHappy December";
+alert(message);
+
+//Que 04
+alert(" السَّلَامُ عَلَيْكُمْ\nIn Arabic.");
+alert("Peace Be Upon You! \nIn English.");
+alert("السلام علیکم \nIn Urdu.");
+
+//Que 05
+ console.log("Hello.. I can run console log message in alert box through JS");
+    alert("Hello.. I can run console log message in alert box through JS");
+
+
+
