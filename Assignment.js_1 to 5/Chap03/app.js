6be144a6fc777adc2583288809d7e53f31997b7a
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2025-12-12 03:51:39 +0500

    Variables For Numbers

---
+++ Assignment.js_1 to 5/Chap03/app.js
@@ -0,0 +1,22 @@
+//Que 01
+var age = 18;
+alert("I'am " + age + " years old");
+
+//Que 02
+var visitCount = "you have visited this site 10 times"
+alert(visitCount);
+
+//Que 03
+var birthYear = 2007;
+document.write("My birth year is " + birthYear + "<br>Data type of my declared variable is number " + birthYear);
+breakline = "<br><br>";
+document.write(breakline);
+
+//Que 04
+var visitorName = "Hanii";
+var prodTitle = "pj's set";
+var quantity = "4";
+document.write(visitorName + " ordered " +  quantity  +  prodTitle +"(M) on Al-Qayoom Cloting Store");
+
+
+
