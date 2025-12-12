2c7849d18abf8417a64f8438aa9c21dc083bb898
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2025-12-12 05:01:53 +0500

    l

---
+++ Assignment.js_1 to 5/chap04/app.js
@@ -0,0 +1,25 @@
+//Que 01
+var statement1 ; var statement2 ; var statement2;
+
+//Que 02 
+var tab = "legal";
+var capsLock = "legal";
+var insert1 = "legal ";
+var ctrl_alt = "legal";
+var pauseBreak = "legal";
+
+// var Shift = "illegal";
+// var 22 = "illegal";
+// var num lock = "illegal";
+// var thelongestvariablearenotecceptedbyjs = "not illegle but try to make short variable names";
+// var #also = "illegal";
+
+
+//Que 03
+document.write(
+    "<h1>Rules for naming JS variables</h1> <br> <br>" + 
+    "Variable names can only contain <u>numbers</u>, <u>$</u>, <u>underscores</u>, <u>number but not before var name</u>. For example $my_1stVariable <br> <br>" +
+    "Variables must begin with a <u>letter</u>, <u>$</u>, <u>_</u>. For example : $name, name , _name. <br> <br>" +
+    "Variables names are case <u>sensitive</u>. <br> <br>" +
+    "Variables names should not be JS <u>keywords</u>. <br>"
+)
