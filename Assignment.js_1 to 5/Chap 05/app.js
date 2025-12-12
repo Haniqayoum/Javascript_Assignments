0f235c6e193b97dcd843d67bb200015c75e6c300
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2025-12-12 05:50:23 +0500

    Mat

---
+++ Assignment.js_1 to 5/Chap 05/app.js
@@ -0,0 +1,55 @@
+//Que 1
+var num1 = 3;
+var num2 = 5;
+var addNum = num1 + num2
+document.write( "<em>Answer 01</em> <br>" +
+    "Sum of 3 + 5 is = " + addNum + "<br> <br>");
+
+//Que 2
+var subt1 = 8;
+var subt2 = 3;
+var subtractNum =subt1 - subt2;
+console.log(subtractNum);
+
+var multiply1 = 6;
+var multiply2 = 2;
+var answer = multiply1 * multiply2;
+console.log(answer);
+
+var modulus = 10 % 8;
+console.log(modulus);
+
+var division = 50 / 2;
+console.log(division);
+
+
+//Que 3
+var num ;
+console.log("value after variable declaration is: ??");
+
+var num = 5;
+console.log("initial value : 5 ");
+
+var newNum = ++num;
+console.log("Value after increment is: " + newNum);
+var addNum = newNum + 7;
+console.log("Value after add 7 is: " + addNum);
+var decrementNum = --addNum;
+console.log("Value after decrement is :" + decrementNum);
+
+var remainder = decrementNum % 3;
+console.log("The remainder is : " + remainder);
+
+
+//Que 4
+var movieTicket = 600;
+var calculate = movieTicket * 5;
+document.write("<em>Answer 4 </EM><br>" +
+    "Total cost to buy 5 tickets to a movie is " +calculate + "PKR"
+);
+
+
+
+
+
+
