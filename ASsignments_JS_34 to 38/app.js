ef5543fbb9191399e58a5be235634a01cc4c8098
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2026-02-11 04:52:27 +0500

    functions

--- ASsignments_JS_34 to 38/app.js
+++ ASsignments_JS_34 to 38/app.js
@@ -2,43 +2,43 @@ var currentdate = new Date();
 console.log(currentdate);
 document.write(currentdate + "<br><br>");
 
-// var firstName = prompt("Enter your first name");
-// var lastName = prompt("Enter your last name");
-// var fullName = firstName.concat(" ", lastName);
-// document.write("Welcome " + fullName + "<br><br>");
+var firstName = prompt("Enter your first name");
+var lastName = prompt("Enter your last name");
+var fullName = firstName.concat(" ", lastName);
+document.write("Welcome " + fullName + "<br><br>");
 
-// function sum(a, b) {
-//     var ans = parseInt(a) + parseInt(b);
-//     console.log(ans);
-//     return ans;
-// }
-// sum(
-//     prompt("Enter first number"),
-//     prompt("Enter second number")
-// );
+function sum(a, b) {
+    var ans = parseInt(a) + parseInt(b);
+    console.log(ans);
+    return ans;
+}
+sum(
+    prompt("Enter first number"),
+    prompt("Enter second number")
+);
 
-// function calculator(){
-//    var num1 = Number(prompt("Number for calculation"));
-//    var num2 = Number(prompt("Number for calculation"));
-//    var operator = prompt("Enter operator ( + , - , * , / )");
+function calculator(){
+   var num1 = Number(prompt("Number for calculation"));
+   var num2 = Number(prompt("Number for calculation"));
+   var operator = prompt("Enter operator ( + , - , * , / )");
 
-//    var operation = "";
-//    if (operator == "+") {
-//     operation = num1 + num2;
-//    } else if (operator == "-") {
-//     operation = num1 - num2;
-//    } else if (operator == "*") {
-//     operation = num1 * num2;
-//    } else if(operator == "/") {
-//     operation = num1 / num2;
-//    } else{
-//     operation = "Invalid Operator";
-//    }
-//    alert (num1 + " " + operator + " " + num2 + "=" + operation);
-//    return operation;
-// }
-// var result = calculator();
-// alert (num1 + " " + operator + " " + num2 + "=" + result);
+   var operation = "";
+   if (operator == "+") {
+    operation = num1 + num2;
+   } else if (operator == "-") {
+    operation = num1 - num2;
+   } else if (operator == "*") {
+    operation = num1 * num2;
+   } else if(operator == "/") {
+    operation = num1 / num2;
+   } else{
+    operation = "Invalid Operator";
+   }
+   alert (num1 + " " + operator + " " + num2 + " = " + operation);
+   return operation;
+}
+var result = calculator();
+//alert (num1 + " " + operator + " " + num2 + "=" + result);
 
 function square (num) {
     var ans = num** 2;
@@ -56,14 +56,14 @@ function factorial(u){
 }
 factorial(5);
 
-// function counting(){
-//     var start =Number(prompt("Counting start from"));
-//     var end = Number(prompt("Counting end at"));
-//     for (var i = start; i <= end; i++){
-//         document.write(i + "<br>");
-//     }
-// }
-// counting();
+function counting(){
+    var start =Number(prompt("Counting start from"));
+    var end = Number(prompt("Counting end at"));
+    for (var i = start; i <= end; i++){
+        document.write(i + "<br>");
+    }
+}
+counting();
 
 function calculateHypotenuse( b , p){
     function calculateSquare(num){
@@ -74,3 +74,84 @@ function calculateHypotenuse( b , p){
 }
 calculateHypotenuse(3,4);
 
+function calculateArea (w , h){
+    var area = w * h;
+    return area;
+}
+console.log(calculateArea(5,10));
+
+function calculateArea (width , height){
+    var A = width * height;
+    return A;
+} 
+var w = 7;
+var h = 4;
+
+console.log(calculateArea(w,h));
+
+
+function palindrome(){
+    var word = prompt("Enter a word to check if it's palindrome or not");
+    var reverse = "";
+    for (var i = word.length -1; i>=0; i--){
+       reverse += word[i]; 
+    }
+    if (word == reverse) {
+        document.write("<br>" +word + " is a palindrome word<br>");
+    } else{
+          document.write(word + " is not a palindrome word" +"<br><br>");
+    }
+}
+palindrome();
+
+function capitalizeWords(store){
+    var result = "";
+    var capialize = true;
+    for ( var i = 0; i < store.length; i++){
+        var char = store[i];
+        if (char === " "){
+            result += char;
+            capialize = true;
+        } else if (capialize){
+            result += char.toUpperCase();
+            capialize = false;
+        } else{
+            result += char;
+        }
+    } 
+    return result;
+}
+document.write("the quick brown fox" + "<br>");
+document.write(capitalizeWords("the quick brown fox") + "<br><br>");
+
+
+function findLongestWord(para){
+    var words = para.split(" ");
+    var longest = "";
+
+    for ( var i = 0; i < words.length; i++){
+        if (words[i].length > longest.length ){
+            longest = words[i];
+        }
+    }
+    return longest;
+}
+//console.log(findLongestWord("Umm-e-hani Abdul Qayoom"));
+//console.log(findLongestWord("Web Development Tutorial"));
+document.write("Web Development Tutorial" + "<br>");
+document.write("Longest word is: " + findLongestWord("Web Development Tutorial") + "<br><br>");
+
+function countVowels(string , letter){
+    var count =0;
+
+    for (var i = 0; i< string.length; i++){
+        if (string[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+var result = countVowels("Ummehani A.Qayoom" , "m");
+console.log(result);
+document.write("Ummehani A.Qayoom" + "<br>");
+document.write("Number of m in the string is " + result + "<br><br>");
