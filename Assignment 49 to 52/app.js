44b63b84c5dc494bf490511118c603729e4402a4
Haniqayoum <ummehaniabdulqayoom@gmail.com>
2026-03-08 07:04:56 +0500

    DOM

--- Assignment 49 to 52/app.js
+++ Assignment 49 to 52/app.js
@@ -9,3 +9,25 @@ for (var i = 0; i< render.length; i++){
 var firstName = document.getElementById("first-name").value = "Hanii"
 var LastName = document.getElementById("last-name").value = "A.Qayoom" 
 var email = document.getElementById("email").value = "ummehaniabdulqayoom@gmail.com"
+
+var nodeType = document.getElementById("form-content").nodeType;
+console.log(nodeType);
+
+var nodeTypeinp = document.getElementById("last-name").nodeType;
+console.log(nodeTypeinp);
+var nodeTypechild = document.getElementById("last-name").childNodes.nodeType;
+console.log(nodeTypechild);
+
+var LastName = document.getElementById("last-name").innerHTML = "Last Name: Qayoom";
+var firstChild = document.getElementById("main-content");
+console.log(firstChild.firstElementChild);
+console.log(firstChild.lastElementChild);
+
+var subling = document.getElementById("lastName");
+console.log(subling.nextElementSibling);
+console.log(subling.previousSibling);
+
+var email = document.getElementById("email");
+console.log(email.nodeType);
+console.log(email.parentNode);
+
